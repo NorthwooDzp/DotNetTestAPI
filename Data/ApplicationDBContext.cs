65b32a6315e65ca208d19c3925947e96683098cf
Volodymyr L <northwoodzp@gmail.com>
2024-10-07 17:40:30 +0300

    L03 Entity framework and db connection

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestWebApi.Models;
+
+namespace TestWebApi.Data;
+
+public class ApplicationDBContext : DbContext
+{
+    public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+    {
+    }
+    
+    public DbSet<Stock> Stocks { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using TestWebApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseNpgsql(connectionString);
+});
 
 var app = builder.Build();
 
